b3e2c5652fe2719062679269c04b2c5f86947cf2
BartoszSmierciak <bartosz.smierciak@gmail.com>
2021-01-27 11:44:00 +0100

    Create timecalc_mean.py

---
+++ timecalc_mean.py
@@ -0,0 +1,17 @@
+import numpy as np
+import statistics
+import time
+
+def time_stat(func, size, ntrials):
+  # the time to generate the random array should not be included
+  data = np.random.rand(size)
+  # modify this function to time func with ntrials times using a new random array each time
+  start = time.perf_counter()
+  res = func(data)
+  seconds = time.perf_counter() - start
+  # return the average run time
+  return seconds
+
+if __name__ == '__main__':
+  print('{:.6f}s for statistics.mean'.format(time_stat(statistics.mean, 10**5, 10)))
+  print('{:.6f}s for np.mean'.format(time_stat(np.mean, 10**5, 1000)))
